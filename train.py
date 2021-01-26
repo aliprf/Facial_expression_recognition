5a03e322a857512dee854ae5b6178e69499c3a44
aliprf <aliprf@gmail.com>
2021-01-26 13:30:07 -0700

    minor changes

--- train.py
+++ train.py
@@ -36,7 +36,7 @@ class Train:
             "./train_logs/fit/" + datetime.now().strftime("%Y%m%d-%H%M%S"))
 
         '''making models'''
-        _lr = 1e-5
+        _lr = 1e-15
         model = self.make_model(arch=arch, w_path=weight_path)
         '''create optimizer'''
         optimizer = self._get_optimizer(lr=_lr)
