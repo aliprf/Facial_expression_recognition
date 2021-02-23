1fcfd722f537e79cafe58e1c465ef65e564a8727
aliprf <aliprf@gmail.com>
2021-02-22 19:45:14 -0700

    creating testset

--- data_helper.py
+++ data_helper.py
@@ -40,8 +40,8 @@ class DataHelper:
             elif ds_type == DatasetType.test:
                 csv_path = AffectnetConf.orig_csv_test_path
                 load_img_path = AffectnetConf.orig_img_path_prefix
-                save_img_path = AffectnetConf.
-                save_anno_path = AffectnetConf.revised_eval_annotation_path
+                save_img_path = AffectnetConf.revised_test_img_path
+                save_anno_path = AffectnetConf.revised_test_annotation_path
                 do_aug = False
 
         csv_data = None
