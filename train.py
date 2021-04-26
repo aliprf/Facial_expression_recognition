ef6ad16aaf1bdb7f6ee5606db2b12a13c8370267
aliprf <aliprf@gmail.com>
2021-04-26 11:51:19 -0600

    train

--- train.py
+++ train.py
@@ -52,7 +52,7 @@ class Train:
         start_train_date = datetime.now().strftime("%Y%m%d-%H%M%S")
 
         '''making models'''
-        _lr = 1e-3
+        _lr = 1e-2
         model = self.make_model(arch=arch, w_path=weight_path)
         '''create optimizer'''
         optimizer = self._get_optimizer(lr=_lr)
