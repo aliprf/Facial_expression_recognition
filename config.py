24e4d95d52e5944fe910cb2d9776cb569b7a3f40
aliprf <aliprf@gmail.com>
2021-04-30 13:19:52 -0600

    push

--- config.py
+++ config.py
@@ -86,7 +86,7 @@ class AffectnetConf:
     # _prefix_path = '/media/sdb4Tb/Ali_data/FER_DS/affectnet'  # --> Aq
     _prefix_path = '/media/data3/ali/FER_DS/affectNet'  # --> zeue
     # _prefix_path = '/media/data2/alip/FER_DS/affectNet'  # --> Atlas
-    _prefix_path = '/media/ali/data/FER/affectNet'  # --> local
+    # _prefix_path = '/media/ali/data/FER/affectNet'  # --> local
 
     orig_csv_train_path = _prefix_path + '/orig/training.csv'
     orig_csv_evaluate_path = _prefix_path + '/orig/validation.csv'
