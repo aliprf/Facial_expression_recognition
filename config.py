7a2094414a5764738b4245a26008c50ded76e5c0
aliprf <aliprf@gmail.com>
2021-05-04 17:01:43 -0600

    custom weighted loss

--- config.py
+++ config.py
@@ -33,10 +33,10 @@ class DatasetType:
 
 
 class LearningConfig:
-    batch_size = 25
+    batch_size = 20
     # batch_size = 30
     # batch_size = 3
-    virtual_batch_size = 150
+    virtual_batch_size = 100
     epochs = 250
     # expression_output_len = 8 # we generated five classes
     embedding_size = 256
