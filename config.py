d144d76ec89cac696cddfff88db5e91ceafa21cc
aliprf <aliprf@gmail.com>
2021-05-12 16:39:26 -0600

    train affect

--- config.py
+++ config.py
@@ -34,10 +34,10 @@ class DatasetType:
 
 
 class LearningConfig:
-    batch_size = 40
-    # batch_size = 30
+    # batch_size = 40
+    batch_size = 20
     # batch_size = 3
-    virtual_batch_size = 250
+    virtual_batch_size = 100
     epochs = 250
     # expression_output_len = 8 # we generated five classes
     embedding_size = 256
