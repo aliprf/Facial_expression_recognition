e32d81eff865414470a812c1efaa4ef841630b63
aliprf <aliprf@gmail.com>
2021-05-20 16:56:18 -0600

    aff create spm mask

--- dataset_dynamic.py
+++ dataset_dynamic.py
@@ -95,7 +95,7 @@ class DynamicDataset:
     def _do_augment(self, img):
         tf_aug = TFAugmentation()
         img = tf_aug.color(img)
-        # img = tf_aug.random_invert_img(img)
+        img = tf_aug.random_invert_img(img)
         # img = tf_aug.random_quality(img)
         # img = tf_aug.random_zoom(img)
         # img = tf_aug.flip(img)
