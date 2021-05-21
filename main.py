e04fd72189a02701333892b395b69a77e4ed36d9
aliprf <aliprf@gmail.com>
2021-05-21 12:57:53 -0600

    train raf

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ if __name__ == '__main__':
     # raf_db.create_synthesized_landmarks(model_file='./ds_136_ef.h5', test_print=False)
     # raf_db.create_masked_image()
 
-    raf_db.create_spatial_masks()
+    # raf_db.create_spatial_masks()
 
     # raf_db = RafDB(ds_type=DatasetType.test)
     # raf_db.create_spatial_masks()
