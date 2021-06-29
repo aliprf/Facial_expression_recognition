e5b61e4094740e8493a09c9b1bc069a205689476
aliprf <aliprf@gmail.com>
2021-06-29 09:40:07 -0600

    create Fer2013

--- main.py
+++ main.py
@@ -93,7 +93,7 @@ if __name__ == '__main__':
     # fer_2013.create_from_orig()
     #
     fer_2013 = RafDB(ds_type=DatasetType.train)
-    fer_2013.report()
+    fer_2013.report(aug=False)
     #
     # fer_2013 = RafDB(ds_type=DatasetType.test)
     # fer_2013.report()
